b9d229bbb0c23b4f1b6e3915cd7cc286be27ac90
sulancheng <suchengkuaile@126.com>
2017-06-05 17:17:33 +0800

    关于gridview的简单ddemo

--- app/src/main/java/com/example/su/myappconectiontwo/MainActivity.java
+++ app/src/main/java/com/example/su/myappconectiontwo/MainActivity.java
@@ -4,11 +4,18 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 
 import com.example.su.myappconectiontwo.shouxieban.ShouXieActivity;
 
 public class MainActivity extends Activity {
+    private String[] iconName = { "通讯录", "日历", "照相机", "时钟", "游戏", "短信", "铃声",
+            "设置", "语音", "天气", "浏览器", "视频" };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,32 @@ public class MainActivity extends Activity {
                 startActivity(new Intent(MainActivity.this, ShouXieActivity.class));
             }
         });
+        GridView gridview = (GridView) findViewById(R.id.gv);
+        gridview.setAdapter(new Adapter());
+    }
+    private class Adapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return iconName.length;
+        }
+
+        @Override
+        public String getItem(int arg0) {
+            return iconName[arg0];
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            return arg0;
+        }
+
+        @Override
+        public View getView(int arg0, View arg1, ViewGroup arg2) {
+            arg1 = getLayoutInflater().inflate(R.layout.wallet_item, null);
+            TextView textView = (TextView) arg1.findViewById(R.id.text) ;
+            textView.setText(iconName[arg0]) ;
+            return arg1;
+        }
     }
 }
